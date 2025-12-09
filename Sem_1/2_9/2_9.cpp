3284dad777a1063f7a77eb8bb1b97866a68a3892
as1mm <vovan114rus@icloud.com>
2025-12-09 13:35:07 +0500

    Update 2_9.cpp

--- Sem_1/2_9/2_9.cpp
+++ Sem_1/2_9/2_9.cpp
@@ -1,1 +1,84 @@
-ы
+#include <iostream>
+
+int main()
+{
+    int n, d;
+    std::cin >> n;
+    d = n / 2;
+    // Рисуем видимую заднюю грань
+    for (int i = 0; i < d; i++)
+    {
+        std::cout << ' ';
+    }
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "* ";
+    }
+    std::cout << std::endl;
+    //Блок верхней диагонали
+    for (int i = 0; i < d - 1; i++)
+    {
+        for (int j = 1; j < d - i; j++)
+        {
+            std::cout << " ";
+        }
+        std::cout << "* ";
+        for (int j = 0; j < n - 2; j++)
+        {
+            std::cout << "  ";
+        }
+        std::cout << "*";
+        for (int j = 0; j < i; j++)
+        {
+            std::cout << " ";
+        }
+        std::cout << "*" << std::endl;
+    }
+    //Передняя верхняя грань + кусок задней грани
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "* ";
+    }
+    for (int i = 0; i < d - 2; i++)
+    {
+        std::cout << " ";
+    }
+    std::cout << "*" << std::endl;
+    //Боковые грани(прямые)
+    for (int i = 0; i < n - d - 1; i++)
+    {
+        std::cout << "* ";
+        for (int j = 0; j < n - 2; j++)
+        {
+            std::cout << "  ";
+        }
+        std::cout << "* ";
+        for (int j = 0; j < d - 2; j++)
+        {
+            std::cout << " ";
+        }
+        std::cout << '*' << std::endl;
+    }
+    //Нижние диагонали
+    for (int i = 0; i < d - 1; i++)
+    {
+        std::cout << "* ";
+        for (int j = 0; j < n - 2; j++)
+        {
+            std::cout << "  ";
+        }
+        std::cout << '*';
+        for (int j = 1; j < d - i - 1; j++)
+        {
+            std::cout << " ";
+        }
+        std:: cout << "*" << std::endl;
+    }
+    //Нижняя грань
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "* ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
